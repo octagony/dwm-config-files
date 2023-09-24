4854f34a417cedfecc1652be9359cd90e3f5d999
octagony <moctagony@gmail.com>
2023-09-25 00:07:34 +0300

    [FEAT] Added more themes

--- dwm/colorschemes/catppuccin.h
+++ dwm/colorschemes/catppuccin.h
@@ -2,4 +2,5 @@ static const char col_gray1[]		= "#1e1d2d";
 static const char col_gray2[] 		= "#1e1d2d";
 static const char col_gray3[] 		= "#d2f7a6";
 static const char col_gray4[] 		= "#1e1d2d";
+static const char col_title[] 		= "#74c7ec";
 static const char col_cyan[] 		= "#cba6f7";
--- dwm/colorschemes/gruvbox.h
+++ dwm/colorschemes/gruvbox.h
@@ -1,5 +1,5 @@
-static const char col_gray1[]		= "#282828";
-static const char col_gray2[] 		= "#282828";
-static const char col_gray3[] 		= "#ebdbb2";
-static const char col_gray4[] 		= "#282828";
-static const char col_cyan[] 		= "#98971a";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#282828";
+static const char col_gray3[]       = "#928374";
+static const char col_gray4[]       = "#282828";
+static const char col_cyan[]        = "#fbf1c7";
