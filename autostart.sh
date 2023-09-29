3f1a0840f3558387b6b17edd847ba00c33c1008c
octagony <moctagony@gmail.com>
2023-09-29 12:27:56 +0300

    [FEAT] Change fonts in dmenu and powerscript

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,14 @@
+# Setup Feh
 feh --bg-fill --randomize ~/.wallpapers/*
+
+#Setup Picom
 "$HOME"/.config/picom/build/src/./picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 500 &
+
+#Exec slstatus
 exec slstatus &
+
+#Exec dunst
 exec dunst &
+
+#Helper for keyboard layout
+setxkbmap -model pc105 -layout us,ru -option grp:alt_shift_toggle
--- dmenu/config.h
+++ dmenu/config.h
@@ -5,7 +5,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"UbuntuMono Nerd Font:weight=bold:size=12:antialias=true:hinting=true"
+	"JetBrainsMono Nerd Font:size=14:antialias=true:hinting=true"
 };
 static const char *prompt      = "You again?";      /* -p  option; prompt to the left of input field */
 static unsigned int lines      = 0;
