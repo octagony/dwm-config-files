fb0849fb5b7ac13ae1847cfe294a81b668839bf2
octagony <moctagony@gmail.com>
2023-11-05 19:54:39 +0300

    [FIX] Fixed browserbookmarks script

--- dwm/scripts/browserbookmarks.sh
+++ dwm/scripts/browserbookmarks.sh
@@ -13,28 +13,28 @@ options=(
 choice=$(echo -e "${options[@]}" | dmenu -c -g 1 -fn "JetBrainsMono Nerd Font" -nb '#1e1e2d' -nf "#abe9b3" -sb "#d5aeea" -sf "#1e1d2d" -shb "#cba6f7" -shf "#585b70" -nhb "#1e1e2e" -nfh "#585b70" -l 15 -h 30 -p 'Bookmarks: ')
 
 case "$choice" in
-DuckDuckGo)
+" DuckDuckGo")
     choice="https://duckduckgo.com/"
     ;;
-Github)
-    choice="https://guthub.com/"
+" Github")
+    choice="https://github.com/"
     ;;
-Youtube)
+" Youtube")
     choice="https://youtube.com/"
     ;;
-Trello)
+" Trello")
     choice="https://trello.com/"
     ;;
-Simplenote)
+" Simplenote")
     choice="https://app.simplenote.com/"
     ;;
-DeepL)
+"󰊿 DeepL")
     choice="https://www.deepl.com/translator"
     ;;
-Piped)
+" Piped")
     choice="https://piped.garudalinux.org/"
     ;;
-Reddit)
+" Reddit")
     choice="https://www.reddit.com/"
     ;;
 *)
