04364c9f254c4be7724abc64da837dc457647fc6
occ <octagony>
2025-06-14 17:57:25 +0300

    [FEAT] Change picom fork from pjulius to fdev31

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,7 @@
 feh --bg-fill --randomize ~/.wallpapers/*
 
 #Setup Picom
-"$HOME"/.config/picom/build/src/./picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 500 &
+"$HOME"/.config/picom/build/src/./picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness-in-tag 10 --animation-stiffness-tag-change 10 --animation-dampening 10 --animation-clamping false &
 
 #Exec slstatus
 exec slstatus &
--- script.sh
+++ script.sh
@@ -2,23 +2,21 @@
 CONF=$HOME/.config
 SUCKLESS=$HOME/suckless
 
-cd "$HOME" || exit 
+cd "$HOME" || exit
 sudo pacman -Sy base-devel xorg-server xorg-xinit libx11 libxinerama libxft libxext libxcb xcb-util-renderutil xcb-util-image pixman dbus libconfig libglvnd pcre libev uthash xorgproto xcb-util meson ninja webkit2gtk dunst pcmanfm flameshot feh brightnessctl pamixer ttf-ubuntu-mono-nerd ttf-jetbrains-mono playerctl imlib2
 
-if [ ! -d "$CONF" ];
-then
-mkdir .config
+if [ ! -d "$CONF" ]; then
+  mkdir .config
 fi
 
-if [ ! -d "$SUCKLESS" ];
-then
-mkdir "$HOME/.config/suckless"
+if [ ! -d "$SUCKLESS" ]; then
+  mkdir "$HOME/.config/suckless"
 fi
 
 cp -r dwm-config-files/{dmenu,dunst,dwm,slock,slstatus,st} "$HOME"/.config/suckless
 
 touch .xinitrc
-echo "exec dwm" > .xinitrc
+echo "exec dwm" >.xinitrc
 
 cd "$HOME"/.config/suckless/st || exit
 sudo make clean install
@@ -39,7 +37,7 @@ cp "$HOME"/dwm-config-files/autostart.sh "$HOME"/.dwm/
 cp -r "$HOME"/dwm-config-files/.wallpapers "$HOME"
 
 cd "$HOME/.config" || exit
-sudo git clone https://github.com/pijulius/picom.git
+sudo git clone https://github.com/fdev31/picom.git
 cd picom/ || exit
 sudo git submodule update --init --recursive
 sudo meson --buildtype=release . build
